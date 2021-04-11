2938ed9339756f649d9066ea96a61f37070cfa58
chirag <chirag.sharma5233@gmai.com>
2021-04-11 16:58:26 +0530

    Last Commit

---
+++ core/src/main/java/mymodel/core/models/MyDetailModel.java
@@ -0,0 +1,90 @@
+package mymodel.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.time.*;
+import java.util.Date;
+
+@Model(adaptables = {Resource.class},
+defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class MyDetailModel {
+
+    @Inject
+    private String firstname;
+
+    @Inject
+    private String lastname;
+
+    @Inject
+    private String gender;
+
+    @Inject
+    private Date dob;
+
+    @Inject
+    private String maritialstatus;
+
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+
+    public String getLastname() {
+        return lastname;
+    }
+
+
+
+    public String getGender() {
+        return gender;
+    }
+
+
+
+    public Date getDob() {
+        return dob;
+    }
+
+
+    public String getMaritialstatus() {
+        return maritialstatus;
+    }
+
+    @PostConstruct
+    public String getAge(){
+        Instant instant = dob.toInstant();
+        ZonedDateTime zone = instant.atZone(ZoneId.systemDefault());
+        LocalDate givenDate = zone.toLocalDate();
+        Period period = Period.between(givenDate,LocalDate.now());
+
+        return period.getYears()+"";
+    }
+
+
+    @PostConstruct
+    public String getHonorific(){
+        if(gender.equals("m")){
+            return "Mr.";
+        }else if(gender.equals("f") && maritialstatus.equals("sin")){
+            return "Ms.";
+        }else
+        {
+            return "Mrs.";
+        }
+    }
+    @PostConstruct
+    public String getfullName(){
+
+        return firstname +" "+lastname;
+
+    }
+
+
+
+}
